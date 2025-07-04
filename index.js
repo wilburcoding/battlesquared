d24b33a9ece5902065d31d86ef89830f4446eec8
Wilbur W <wilburwei.2008@gmail.com>
2025-07-04 09:47:30 -0400

    A little more secure

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const app = express();
 const path = require('path');
 const http = require('http');
 const { Server } = require("socket.io");
+require('dotenv').config();
 
 const server = http.createServer(app);
 const io = new Server(server);
 const sqlite3 = require('sqlite3').verbose(); 
 const db = new sqlite3.Database('./main.sqlite');
-
 io.on("connection", (socket) => {
   console.log("User Connected: "+ socket.id);
+  socket.emit("apikey", process.env.API_KEY)
   socket.on("message", (msg) => {
     console.log(msg);
   });
+  // socket.on("apikey",() => {
+  //   socket.emit("apikey", process.env.API_KEY)
+  // })
   socket.on("login", (uid) => {
     console.log(uid);
     db.get("SELECT * FROM users WHERE uid = ?", [uid], (err, row) => {
