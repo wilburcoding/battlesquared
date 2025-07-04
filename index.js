222a947bf8104a50db04361496dc24c5f92d183f
Wilbur W <wilburwei.2008@gmail.com>
2025-07-03 22:28:35 -0400

    Set up base socket server stuff

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http');
+const { Server } = require("socket.io");
+
 const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("User Connected: "+ socket.id);
+  socket.on("message", (msg) => {
+    console.log(msg);
+  });
+  socket.on("disconnect", (socket) => {
+    console.log("User Disconnected: " + socket.id);
+  });
+});
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/static/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'static')));
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
